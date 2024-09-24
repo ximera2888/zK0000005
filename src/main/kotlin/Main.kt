5bc922adfe52136ceec063f209732b08ae560954
katya <ximera2888@gmail.com>
2024-09-24 11:52:01 +0300

    z6-2v2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -11,19 +11,24 @@ data class Post(
     val can_delete: Boolean?=true
 ) {}
 //data class attachmentPost():Array
- class AttachmentPhoto(override var id:Int, override val type:String="Photo", override val title:String ):Attachment{
+data class Photo(val id: Int, val album_id:Int=0, val owner_id:Int)
+data class Video(val id: Int,val owner_id:Int, val duration:Int )
+data class Audio(val id: Int, val owner_id:Int, val artist:String="Noname",val album_id:Int=0)
+data class File(val id: Int,  val owner_id:Int, val title: String, val size:Int)
+data class Url(val url: String, val title: String,val caption: String,)
+ class AttachmentPhoto(override var id:Int, override val type:String="Photo", override val title:String, val photo:Photo ):Attachment{
 
 }
-class AttachmentVideo(override var id:Int, override val type:String="Video", override val title:String ):Attachment{
+class AttachmentVideo(override var id:Int, override val type:String="Video", override val title:String,val video:Video  ):Attachment{
 
 }
-class AttachmentAudio(override var id:Int, override val type:String="Audio", override val title:String ):Attachment{
+class AttachmentAudio(override var id:Int, override val type:String="Audio", override val title:String,val audio:Audio ):Attachment{
 
 }
-class AttachmentFile(override var id:Int, override val type:String="File", override val title:String ):Attachment{
+class AttachmentFile(override var id:Int, override val type:String="File", override val title:String,val file:File ):Attachment{
 
 }
-class AttachmentUrl(override var id:Int, override val type:String="Url", override val title:String ):Attachment{
+class AttachmentUrl(override var id:Int, override val type:String="Url", override val title:String ,val url:Url):Attachment{
 
 }
 //class Photo(override var id: Int = 1, override override val type: String): AttachmentPhoto
